c4396d8cd6c9961388f1c8296de6c80008578743
Eric Peterson <eric@ericpeterson.mn>
2013-09-05 13:03:48 -0500

    Initial import

---
+++ examples/simple_web_server.js
@@ -0,0 +1,14 @@
+/*
+
+Copied from node.js home page.
+
+Used to test node.js install
+
+*/
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
